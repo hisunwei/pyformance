1bd63f46cda5f825d6765562b7a7d69d171646fa
孙微 <sunwei1@9fbank.cc>
2018-08-09 12:49:44 +0800

    add customize tag for metrics

--- pyformance/reporters/influx.py
+++ pyformance/reporters/influx.py
@@ -25,18 +25,23 @@ class InfluxReporter(Reporter):
     """
     InfluxDB reporter using native http api
     (based on https://influxdb.com/docs/v1.1/guides/writing_data.html)
+    metrics_tag_keys for some customize tag for some metric, like url, http status code or some dynamic error code
+    that is not convinent to batch query in inlfuxdb if you put url/code like metric into table name.
+    instead we use influxdb tag feature to support this
+    while name a matric use a prefix '^' to mark the tags, use '_' to split the  metric name and tag values, ex: "^_httpRequest_url1_200"
+    a metrics tag in metrics_tag_keys is need, ex:  metrics_tag_keys = {"httpRequest": ["url", "status"]},
     """
-
-    def __init__(self, registry=None, reporting_interval=5, prefix="",tags={},
+    def __init__(self, registry=None, reporting_interval=5, prefix="",tags={}, metrics_tag_keys = {},
                  database=DEFAULT_INFLUX_DATABASE, server=DEFAULT_INFLUX_SERVER,
                  username=DEFAULT_INFLUX_USERNAME,
                  password=DEFAULT_INFLUX_PASSWORD,
                  port=DEFAULT_INFLUX_PORT, protocol=DEFAULT_INFLUX_PROTOCOL,
                  autocreate_database=False, clock=None):
-        self.tagsStr = ",".join(["%s=%s" % (k, tags[k]) for k in tags.keys()])
+        self.tags_str = ",".join(["%s=%s" % (k, tags[k]) for k in tags.keys()])
         super(InfluxReporter, self).__init__(
             registry, reporting_interval, clock)
         self.prefix = prefix
+        self.metrics_tag_keys = metrics_tag_keys
         self.database = database
         self.username = username
         self.password = password
@@ -73,11 +78,29 @@ class InfluxReporter(Reporter):
                 table = key
             else:
                 table = "%s.%s" % (self.prefix, key)
+
+            if str(key).startswith("^_"):
+                slices = str(key).split("_")
+                #0 is ^ prefix,  1 is metric name, left is tag values
+                if len(slices) >= 0:
+                    metric_name = slices[1]
+                    tag_keys = self.metrics_tag_keys[metric_name]
+                    if len(slices) == len(tag_keys) +1 :
+                        metric_tags_str = ",".join(["%s=%s" % (slices[i+1], tag_keys[i]) for i in range(0, len(tag_keys))])
+                        table = "%s,%s" % (table, metric_tags_str)
+                    else:
+                        LOG.warn("skip metric tags parse error, metric key:" + str(key))
+                        continue
+                else:
+                    LOG.warn("skip metric tags parse error, metric key:" + str(key))
+                    continue
+
             values = ",".join(["%s=%s" % (k, v if type(v) is not str \
                                                else '"{}"'.format(v))
                               for (k, v) in metric_values.items()])
-            if self.tagsStr.__len__()>0:
-                table = "%s,%s" % (table, self.tagsStr)
+            if self.tags_str.__len__()>0:
+                table = "%s,%s" % (table, self.tags_str)
+
             line = "%s %s %s" % (table, values, timestamp)
             post_data.append(line)
         post_data = "\n".join(post_data)
